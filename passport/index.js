34fb9ef112c0138adbd6fa8f5146677dd8b962cf
young <tlszn121@sookmyung.ac.kr>
2021-07-21 19:26:22 +0900

    01commit

--- passport/index.js
+++ passport/index.js
@@ -2,7 +2,7 @@ const passport=require('passport');
 const local=require('./localStrategy');
 const kakao=require('./kakaoStrategy');
 const User=require('../models/user');
-const Post=require('../models/post');
+const Room=require('../models/room');
 
 module.exports=()=>{
     passport.serializeUser((user,done)=>{ //로그인 시 실행. req.session객체에 사용자정보객체를 세션에 id를 통해 저장
@@ -12,6 +12,10 @@ module.exports=()=>{
     passport.deserializeUser((id,done)=>{ //매 요청 시 실행. id를 받아 사용자정보객체를 불러옴(req.user)
         User.findOne({
             where:{id},
+            include:[{
+                model:Room,
+                attributes:['id'],
+            }],
         })
         .then(user=>done(null,user))
         .catch(err=>done(err));
